f02e0426d48a75783e54395f93637094a0a30236
sintrb <sintrb@gmail.com>
2016-05-02 13:04:51 +0800

    fix

--- README.rst
+++ README.rst
@@ -1,6 +1,54 @@
 runwsgi
 ===============
-A simple runner for Python wsgi application. Like Gunicorn but can run on Windows. It's to simple, so do not use at production environment.
+A simple runner for Python wsgi application. Like Gunicorn but can run on Windows. It's to simple, so **do not use at production environment**.
+
+Install
+===============
+::
+
+ pip install runwsgi
+
 
 Use
 ===============
+run command:
+
+::
+
+ runwsgi -b 128.0.0.1:8000 -d /home/robin/demo/ -p "robin" demo:app
+
+on Windows use:
+::
+
+ runwsgi.bat -b 128.0.0.1:8000 -d C:\Users\Administrator\Desktop\demo\ -p "robin" demo:app
+
+if can't find runwsgi command, try:
+::
+
+ python -m runwsgi -b 128.0.0.1:8000 -d /home/robin/demo/ -p "robin" demo:app
+
+
+open broswer with url **http://127.0.0.1:8000**
+	
+
+Other tips
+===============
+1. -b
+::
+
+ set bind address
+
+2. -d
+::
+
+ set you wsgi work dir
+
+3. -p
+::
+
+ set environment variable of WSGI_PARAMS, and then you can use "os.environ.get('WSGI_PARAMS')" to get the parameter.
+
+4. wsgi application
+::
+
+ model:application
--- runwsgi/runwsgi.py
+++ runwsgi/runwsgi.py
@@ -12,14 +12,15 @@ import sys, os
 from wsgiref.simple_server import make_server
 
 def print_help():
-    print 'Usage: python -m runwsgi [-b host:port] [-p "params"] model:applicaon'
+    print 'Usage: python -m runwsgi [-d workdir] [-b host:port] [-p "params"] model:applicaon'
     print 'Report bugs to <sintrb@gmail.com>'
 
 def main():
     import getopt
-    opts, args = getopt.getopt(sys.argv[1:], "hb:p:")
+    opts, args = getopt.getopt(sys.argv[1:], "hb:p:d:")
     host = '0.0.0.0'
-    port = 8080
+    port = 8000
+    workdir = os.getcwd()
     for opt, arg in opts:
         if opt == '-b':
             if ':' in arg:
@@ -29,6 +30,8 @@ def main():
                 port = int(arg)
         elif opt == '-p':
             os.environ.setdefault('WSGI_PARAMS', arg)
+        elif opt == '-d':
+            workdir = arg
         elif opt == '-h':
             print_help()
             exit()
@@ -37,7 +40,7 @@ def main():
         exit(-1)
     app_path = args[0]
     modules, application = app_path.split(':')
-    sys.path.insert(0, os.getcwd())
+    sys.path.insert(0, workdir)
     module = __import__(modules)
     for m in modules.split('.')[1:]:
         module = getattr(module, m)
